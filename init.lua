de7d327c5c4c7af34dbd2ca1eb171185e8f90f21
rolf malthe andersen <rma@it-minds.dk>
2024-05-01 09:53:49 +0200

    init

--- init.lua
+++ init.lua
@@ -224,6 +224,7 @@ vim.opt.rtp:prepend(lazypath)
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
+require 'custom'
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
@@ -877,7 +878,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
---
+++ lua/custom/init.lua
@@ -0,0 +1,1 @@
+vim.keymap.set('n', '<C-s>', '<cmd>w<cr>', { silent = true })
